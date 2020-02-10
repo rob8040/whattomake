a94f22ce20934d745d1991dca0ca6fc6f02a5f82
rob8040 <rob8040@hotmail.com>
2020-02-10 11:37:44 -0500

    created user table and model validations

--- db/migrate/20200210160908_devise_create_users.rb
+++ db/migrate/20200210160908_devise_create_users.rb
@@ -6,6 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :username,           null: false, default: ""
+      t.text :about
+      t.text :profile_photo,        null: false
 
       ## Recoverable
       t.string   :reset_password_token
