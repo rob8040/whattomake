068826c1754fa768c500b1c6feb6d0333fa42bde
rob8040 <rob8040@hotmail.com>
2020-02-11 16:24:13 -0500

    created basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "rob@email.com", username: "foodmaster", profile_photo: "image.jpg", password: "123456")
+Ingredient.create(user_id: 1, ingredients: "bacon, egg, cheese")
+Recipe.create(user_id: 1, ingredient_id: 1, picture: "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Bacon-Egg-Cheese-Bagel.jpg", ingredients: "bacon, egg, cheese, bagel", instructions: "cook eggs and bacon. put on bagel. add cheese", rating: 7, review: "pretty simple and delicious!")
