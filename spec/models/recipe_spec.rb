61869f8415bfec304f0b1b6991e401683674165e
rob8040 <rob8040@hotmail.com>
2020-02-12 13:28:49 -0500

    tested recipe model

---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  it { is_expected.to validate_presence_of(:picture)}
+  it { is_expected.to validate_presence_of(:ingredients)}
+  it { is_expected.to validate_presence_of(:instructions)}
+  # binding.pry
+  it { is_expected.to validate_numericality_of(:rating)}
+end
