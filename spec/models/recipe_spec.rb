27ba7258dc02bb592ce7905da1792d1a163c8631
rob8040 <rob8040@hotmail.com>
2020-02-12 13:35:23 -0500

    removed debugger

---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  it { is_expected.to validate_presence_of(:picture)}
+  it { is_expected.to validate_presence_of(:ingredients)}
+  it { is_expected.to validate_presence_of(:instructions)}
+  it { is_expected.to validate_numericality_of(:rating)}
+end
