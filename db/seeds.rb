500ffab3d52803d7af00c2150873f740a5bfc1fe
rob8040 <rob8040@hotmail.com>
2020-02-13 14:17:48 -0500

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(email: "rob@email.com", username: "foodmaster", profile_photo: "image.jpg", password: "123456")
+User.create(email: "user@email.com", username: "foodn00b", profile_photo: "image2.jpg", password: "123456")
 Ingredient.create(user_id: 1, ingredients: "bacon, egg, cheese")
-Recipe.create(user_id: 1, ingredient_id: 1,name: "bacon egg and cheese sandwich", picture: "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Bacon-Egg-Cheese-Bagel.jpg", ingredients: "bacon, egg, cheese, bagel", instructions: "cook eggs and bacon. put on bagel. add cheese", rating: 7, review: "pretty simple and delicious!")
+Ingredient.create(user_id: 2, ingredients: "peanut butter and jelly")
+Recipe.create(user_id: 1, ingredient_id: 1, name: "bacon egg and cheese sandwich", picture: "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Bacon-Egg-Cheese-Bagel.jpg", ingredients: "bacon, egg, cheese, bagel", instructions: "cook eggs and bacon. put on bagel. add cheese", rating: 7, review: "pretty simple and delicious!")
+Recipe.create(user_id: 2, ingredient_id: 2, name: "pbj", picture: "https://www.baycare.net/media/3906/istock-1131185755.jpg", ingredients: "peanut butter, bread, jelly", instructions: "spread pb and jelly on bread slices", rating: 6, review: "The ol standby")
