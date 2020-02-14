893e298eea9b2eaf14785e02e56d7edfc864f9a0
rob8040 <rob8040@hotmail.com>
2020-02-14 14:21:53 -0500

    WIP 3rd api, API down for today

--- app/controllers/api/v1/ingredients_controller.rb
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -12,7 +12,18 @@ class Api::V1::IngredientsController < ApplicationController
     if current_user
       ingredients = Ingredient.new(ingredient_params)
       ingredients.user = current_user
+      array = []
+      base_url = "https://api.spoonacular.com/recipes/findByIngredients?ingredients="
+      split_query = ingredients.ingredients.split(", ")
+      array << split_query.shift
+      plus = split_query.map { |x| "+" + x }
+      array.concat(plus)
+      final = array.join(",")
+      search_url = base_url += (final + "&number=3")
+      search = search_url += ("&apiKey=#{ENV["SPOON_KEY"]}")
 
+      # response = Faraday.get(search)
+      binding.pry
       if ingredients.save
         render json: { ingredients: ingredients}
       else
--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,4 +2,7 @@ class Api::V1::RecipesController < ApplicationController
   def index
     render json: Recipe.all
   end
+
+  def show
+  end
 end
