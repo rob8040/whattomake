42fe204de374f0436e7e44d775999df2e097fb9c
rob8040 <rob8040@hotmail.com>
2020-02-18 17:52:04 -0500

    show page now renders dynamic recipe information

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -4,6 +4,18 @@ class Api::V1::RecipesController < ApplicationController
   end
 
   def show
-    
+    recipe_id = params["id"]
+    response = Faraday.get("https://api.spoonacular.com/recipes/#{recipe_id}/information?includeNutrition=false&apiKey=#{ENV["SPOON_KEY"]}")
+    parsed_response = JSON.parse(response.body)
+    ingredient_list = parsed_response["extendedIngredients"]
+    ingredients_array = []
+    ingredient_list.each do |ingredient|
+      ingredients_array << ingredient["original"]
+    end
+    name = parsed_response["title"]
+    picture = parsed_response["image"]
+    instructions = parsed_response["instructions"]
+
+    render json: { ingredients_array: ingredients_array, name: name, picture: picture, instructions: instructions}
   end
 end
