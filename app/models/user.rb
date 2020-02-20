05d06ce4f84f34414c0adf7a1d90430a3f2d9784
rob8040 <rob8040@hotmail.com>
2020-02-20 09:18:52 -0500

    removed photo and about section from table and form

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,4 @@ class User < ApplicationRecord
   has_many :recipes
 
   validates :username, presence: true
-  validates :profile_photo, presence: true
 end
--- db/migrate/20200210160908_devise_create_users.rb
+++ db/migrate/20200210160908_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :encrypted_password, null: false, default: ""
       t.string :username,           null: false, default: ""
       t.text :about
-      t.text :profile_photo,        null: false
+      t.text :profile_photo
 
       ## Recoverable
       t.string   :reset_password_token
